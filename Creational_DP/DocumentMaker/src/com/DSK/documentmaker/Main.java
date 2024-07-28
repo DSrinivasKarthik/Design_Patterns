66ffeb43e9c607263e524624689f4d76613d37b1
D Srinivas Karthik <dsrinivaskarthik@gmail.com>
2024-07-28 16:30:11 +0530

    Creational Design Pattern - Factory Pattern

--- Creational_DP/DocumentMaker/src/com/DSK/documentmaker/Main.java
+++ Creational_DP/DocumentMaker/src/com/DSK/documentmaker/Main.java
@@ -1,11 +1,8 @@
 package com.DSK.documentmaker;
 
 import java.util.Scanner;
-import java.util.logging.Logger;
 
 public class Main {
-    private static final Logger logger = Logger.getLogger(Main.class.getName());
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
