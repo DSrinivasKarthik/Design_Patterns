9841120cf8d59aa8b66bb18da204d27f483e3759
D Srinivas Karthik <dsrinivaskarthik@gmail.com>
2024-07-28 14:58:54 +0530

    Structural Design Pattern - Adapter Pattern

---
+++ Structural_DP/MediaPlayer/src/com/DSK/mediaplayer/AdvancedMediaPlayer.java
@@ -0,0 +1,6 @@
+package com.DSK.mediaplayer;
+
+public interface AdvancedMediaPlayer {
+    void playVlc(String fileName);
+    void playMp4(String fileName);
+}
---
+++ Structural_DP/MediaPlayer/src/com/DSK/mediaplayer/AudioPlayer.java
@@ -0,0 +1,21 @@
+package com.DSK.mediaplayer;
+
+public class AudioPlayer implements MediaPlayer {
+
+    MediaAdapter mediaAdapter;
+
+    @Override
+    public void play(String audioType, String fileName) {
+        // Built-in support to play mp3 music files
+        if (audioType.equalsIgnoreCase("mp3")) {
+            System.out.println("Playing mp3 file. Name: " + fileName);
+        }
+        // MediaAdapter provides support to play other file formats
+        else if (audioType.equalsIgnoreCase("mp4") || audioType.equalsIgnoreCase("vlc")) {
+            mediaAdapter = new MediaAdapter(audioType);
+            mediaAdapter.play(audioType, fileName);
+        } else {
+            System.out.println("Invalid media. " + audioType + " format not supported");
+        }
+    }
+}
